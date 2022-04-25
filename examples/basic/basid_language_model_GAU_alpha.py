ff6c1a1f052a4cfa858db3bf06d7eac811266be9
LB <tongjilibo@163.com>
2022-04-25 20:30:48 +0800

    add GAU examples

--- examples/basic/basid_language_model_GAU_alpha.py
+++ examples/basic/basid_language_model_GAU_alpha.py
@@ -1,5 +1,6 @@
 #! -*- coding: utf-8 -*-
 # 基础测试：GAU_alpha的mlm预测，和bert4keras版本比对一致
+# 测试中长文本效果明显高于短文本效果
 
 from bert4torch.models import build_transformer_model
 from bert4torch.tokenizers import Tokenizer
@@ -15,8 +16,8 @@ dict_path = 'F:/Projects/pretrain_ckpt/gau/[sushen-torch]--chinese_GAU-alpha-cha
 tokenizer = Tokenizer(dict_path, do_lower_case=True)
 model = build_transformer_model(config_path, checkpoint_path, model='gau_alpha', with_mlm='linear')  # 建立模型，加载权重
 
-token_ids, segments_ids = tokenizer.encode("科学技术是第一生产力")
-token_ids[3] = token_ids[4] = tokenizer._token_mask_id
+token_ids, segments_ids = tokenizer.encode("近期正是上市公司财报密集披露的时间，但有多家龙头公司的业绩令投资者失望")
+token_ids[5] = token_ids[6] = tokenizer._token_mask_id
 print(''.join(tokenizer.ids_to_tokens(token_ids)))
 
 tokens_ids_tensor = torch.tensor([token_ids])
@@ -26,5 +27,5 @@ segment_ids_tensor = torch.tensor([segments_ids])
 model.eval()
 with torch.no_grad():
     _, probas = model([tokens_ids_tensor, segment_ids_tensor])
-    result = torch.argmax(probas[0, 3:5], dim=-1).numpy()
+    result = torch.argmax(probas[0, 5:7], dim=-1).numpy()
     print(tokenizer.decode(result))
