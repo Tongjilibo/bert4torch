7b6e2d43a8da2ab4844b7edbab3141aed3ddc5ca
李波 <lb@libos-MacBook-Pro.local>
2022-07-08 10:55:46 +0800

    modify finFAQ examples

--- examples/sentence_embedding/FinanceFAQ/README.md
+++ examples/sentence_embedding/FinanceFAQ/README.md
@@ -1,9 +1,21 @@
 # 召回+排序两阶段模型
+金融场景FAQ解决方案
+
 ## 建模思路
 1. 阶段一：MultiNegativeRankingLoss来做有监督的语义相似度任务
 2. 利用1阶段训练好的模型，用向量相似度为所有的相似问q_sim召回最相近的K个标准问q_std_pred, 其中等于q_std的为正样本，不等于的为困难负样本
-3. 阶段二：CosineSimilarityLoss来做有监督的语义相似度任务
+3. 阶段二：ContrastiveLoss来做有监督的语义相似度任务
 4. 预测：一个query通过阶段一模型找到topK个标问q_std, 然后通过阶段二模型从topK个标问中找到最可能的标问
 
 ## 优缺点分析
 - 阶段一的训练自动为阶段二模型构造困难样本，类似于Boosting的思想，进一步提升准确率
+
+## 文件说明
+| 文件名 | 文件描述 |
+| ----  |  ----  |
+| task_sentence_embedding_FinanceFAQ_step1_0.ipynb | 阶段一模型数据生成 |
+| task_sentence_embedding_FinanceFAQ_step1_1.py | 阶段一模型训练 |
+| task_sentence_embedding_FinanceFAQ_step2_0.ipynb | 阶段二模型数据生成 |
+| task_sentence_embedding_FinanceFAQ_step2_1.ipynb | 阶段二模型训练 |
+| task_sentence_embedding_FinanceFAQ_step3_predict.ipynb | 模型效果评估 |
+| task_sentence_embedding_FinanceFAQ_step3_inference.ipynb | 单条样本推理 |
--- examples/sentence_embedding/FinanceFAQ/utils.py
+++ examples/sentence_embedding/FinanceFAQ/utils.py
@@ -1,6 +1,7 @@
 import torch
 from torch import Tensor
 import numpy as np
+import pandas as pd
 
 def pytorch_cos_sim(a: Tensor, b: Tensor):
     if not isinstance(a, torch.Tensor):
@@ -54,7 +55,32 @@ def cos_sim4matrix(arr, brr):
 def cos_sim4matrix_2(arr, brr):
     return (arr.dot(brr.T) / (np.sqrt(np.sum(arr * arr)) * np.sqrt(np.sum(brr * brr, axis=1))))
 
-def cal_performance(texts_embeddings_dict, q_std_sentence_embeddings, q_std_list, texts, df_eval, K=20):
+def read_q_std_q_corpus(q_std_file, q_std_vectors_file, q_corpus_file, q_corpus_vectors_file):
+    '''读取q_std、q_corpus语料和向量
+    '''
+    print('读取标准问及其向量'.center(60, '-'))
+    q_std_list = pd.read_csv(q_std_file, sep="\t", names=['c']).c.tolist()
+    q_std_sentence_embeddings = np.load(q_std_vectors_file)
+    print('标准问shape：', q_std_sentence_embeddings.shape, len(q_std_list))
+
+    print('读取所有语料及其向量'.center(60, '-'))
+    q_all = pd.read_csv(q_corpus_file, sep="\t", names=['c']).c.tolist()
+    q_all_sentence_embeddings = np.load(q_corpus_vectors_file)
+    q_all_sentence_embeddings_dict = {q_all[i]: q_all_sentence_embeddings[i] for i in range(0, len(q_all))}
+    print('所有语料shape', q_all_sentence_embeddings.shape, len(q_all))
+    return q_std_list, q_std_sentence_embeddings, q_all, q_all_sentence_embeddings_dict
+
+def cal_performance(model, q_all_sentence_embeddings_dict, q_std_sentence_embeddings, q_std_list, df_eval, K=20):
+    '''计算召回topK的指标
+    '''
+    texts = df_eval.q_sim.tolist()
+    texts_in = [v for v in texts if v in q_all_sentence_embeddings_dict.keys()]
+    texts_out = [v for v in texts if v not in q_all_sentence_embeddings_dict.keys()]
+    texts_out_embeddings = model.encode(texts_out) if texts_out else []
+    texts_embeddings_dict_1 = {texts_in[i]: q_all_sentence_embeddings_dict[texts_in[i]] for i in range(0, len(texts_in))}
+    texts_embeddings_dict_2 = {texts_out[i]: texts_out_embeddings[i] for i in range(0, len(texts_out))}
+    texts_embeddings_dict = {**texts_embeddings_dict_1, **texts_embeddings_dict_2}
+
     print(f'计算相似度 K= {K}'.center(60, '-'))
     df_eval['ifin'] = df_eval.q_std.apply(lambda v: 1 if v in q_std_list else 0)
     print("目标语料标问是否存在：——>", df_eval.groupby("ifin")["ifin"].count())
