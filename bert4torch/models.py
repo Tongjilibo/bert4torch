6f18d0a3447772d1cb9b6374cb76febdbf915d56
LB <tongjilibo@163.com>
2022-09-01 21:02:34 +0800

    fix bug

--- bert4torch/models.py
+++ bert4torch/models.py
@@ -209,7 +209,7 @@ class BaseModel(nn.Module):
                 callback.on_dataloader_end()
 
     def fit(self, train_dataloader, steps_per_epoch=None, epochs=1, grad_accumulation_steps=1, callbacks=[]):
-        if hasattr(train_dataloader.dataset, '__len__'):
+        if not hasattr(train_dataloader, '__len__'):
             assert steps_per_epoch is not None, 'Either train_dataloader.dataset has attr "__len__" or steps_per_epoch is not None'
 
         self.steps_per_epoch = len(train_dataloader) if steps_per_epoch is None else steps_per_epoch
