6b63ee4f5812dd877ad0f76469ce9b8a5f246f21
李波 <lb@libos-MacBook-Pro.local>
2022-12-15 10:28:36 +0800

    add instructions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ author = 'Tongjilibo'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark','sphinx_markdown_tables',
+extensions = ['recommonmark',
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
