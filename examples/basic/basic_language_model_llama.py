b11f58e6625d050c73b049a7e5153d6703168220
LB <tongjilibo@163.com>
2023-03-18 00:33:17 +0800

    fix model.half()

--- examples/basic/basic_language_model_llama.py
+++ examples/basic/basic_language_model_llama.py
@@ -1,6 +1,12 @@
 #! -*- coding: utf-8 -*-
 # 基本测试：llama的7b模型的测试, fp32精度的单卡占用约27g，fp16的显存占用约14g
-# 使用前需要先进行权重转换 https://github.com/Tongjilibo/bert4torch/blob/master/examples/convert_script/convert_llama_facebook.py
+# 使用前需要安装最新版本的bert4torch并进行权重转换 https://github.com/Tongjilibo/bert4torch/blob/master/examples/convert_script/convert_llama_facebook.py
+
+# 0. Install lastest bert4torch: `pip install git+https://www.github.com/Tongjilibo/bert4torch.git` or git clone
+# 1. Download weights：[Github](https://github.com/facebookresearch/llama) | [huggingface](https://huggingface.co/decapoda-research/llama-7b-hf) | [torrent](https://pan.baidu.com/s/1yBaYZK5LHIbJyCCbtFLW3A?pwd=phhd)，本人实现是基于第三种
+# 2. Convert weights：https://github.com/Tongjilibo/bert4torch/blob/master/examples/convert_script/convert_llama_facebook.py
+# 3. Inference script：https://github.com/Tongjilibo/bert4torch/blob/master/examples/basic/basic_language_model_llama.py
+# 4. VRAM request in single gpu：fp32 27G, fp16 14g
 
 import torch
 from bert4torch.models import build_transformer_model
@@ -17,8 +23,6 @@ tokenizer = SpTokenizer(spm_path, token_start='<s>', token_end=None, keep_accent
 model = build_transformer_model(config_path=config_path, checkpoint_path=checkpoint_path, model='llama').half().to(device)  # 建立模型，加载权重
 
 class ArticleCompletion(AutoRegressiveDecoder):
-    """基于随机采样的文章续写
-    """
     @AutoRegressiveDecoder.wraps(default_rtype='probas')
     def predict(self, inputs, output_ids, states):
         token_ids = torch.cat([inputs[0], output_ids], 1)
