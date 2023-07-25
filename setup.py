af6221ffb353d6d4988584600a52faa2473dc78e
LB <tongjilibo@163.com>
2023-07-26 00:13:39 +0800

    v0.3.1.post2

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='bert4torch',
-    version='v0.3.1',
+    version='v0.3.1.post2',
     description='an elegant bert4torch',
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='MIT Licence',
     url='https://github.com/Tongjilibo/bert4torch',
     author='Tongjilibo',
-    install_requires=['torch>1.6', 'torch4keras==0.1.0'],
+    install_requires=['torch>1.6', 'torch4keras==0.1.0.post2'],
     packages=find_packages()
 )
