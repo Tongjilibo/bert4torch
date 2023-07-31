eafc1ec7251c22e7f13ebcf0899b5b90eb865fec
Tongjilibo <lb@libos-MacBook-Pro.local>
2023-07-31 15:48:00 +0800

    rename lm_head

--- examples/basic/chatglm/basic_language_model_chatglm_stream_multigpus.py
+++ examples/basic/chatglm/basic_language_model_chatglm_stream_multigpus.py
@@ -7,7 +7,7 @@ from basic_language_model_chatglm_stream import *
 
 def auto_configure_device_map(num_gpus: int) -> Dict[str, int]:
     # embeddings.word_embeddings 占用1层
-    # LayerNormFinal 和 dense 占用1层
+    # LayerNormFinal 和 lm_head 占用1层
     # transformer.layers 占用 28 层
     # 总共30层分配到num_gpus张卡上
     num_trans_layers = 28
@@ -15,11 +15,11 @@ def auto_configure_device_map(num_gpus: int) -> Dict[str, int]:
 
     # bugfix: 在linux中调用torch.embedding传入的weight,input不在同一device上,导致RuntimeError
     # windows下 model.device 会被设置成 embeddings.word_embeddings.device
-    # linux下 model.device 会被设置成 dense.device
+    # linux下 model.device 会被设置成 lm_head.device
     # 在调用chat或者stream_chat时,input_ids会被放到model.device上
     # 如果embeddings.word_embeddings.device和model.device不同,则会导致RuntimeError
-    # 因此这里将embeddings.word_embeddings,LayerNormFinal,dense都放到第一张卡上
-    device_map = {'embeddings.word_embeddings': 0, 'LayerNormFinal': 0, 'dense': 0}
+    # 因此这里将embeddings.word_embeddings,LayerNormFinal,lm_head都放到第一张卡上
+    device_map = {'embeddings.word_embeddings': 0, 'LayerNormFinal': 0, 'lm_head': 0}
     used = 2
     gpu_target = 0
     for i in range(num_trans_layers):
