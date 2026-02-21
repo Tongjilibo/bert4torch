06061ddc1b6c4d1616112e814f1910fce3365976
Tongjilibo <tongjilibo@163.com>
2026-02-21 22:23:41 +0800

    修改readme

--- examples/basic/deepseek/basic_language_model_deepseek_ocr.py
+++ examples/basic/deepseek/basic_language_model_deepseek_ocr.py
@@ -5,17 +5,11 @@
 from bert4torch.models import build_transformer_model
 import torch
 
-model_dir = '/data/pretrain_ckpt/deepseek-ai/DeepSeek-OCR'
-image_size = 640
-crop_thread = 640
-dynamic_preprocess_max_num = 9
-add_image_token_id = True
+# model_dir = '/data/pretrain_ckpt/deepseek-ai/DeepSeek-OCR'
+# add_image_token_id = True
 
-# model_dir = '/data/pretrain_ckpt/deepseek-ai/DeepSeek-OCR-2'
-# image_size = 768
-# crop_thread = 768
-# dynamic_preprocess_max_num = 6
-# add_image_token_id = False
+model_dir = '/data/pretrain_ckpt/deepseek-ai/DeepSeek-OCR-2'
+add_image_token_id = False
 
 prompt = "<image>\n<|grounding|>Convert the document to markdown."
 image_file = '/data/pretrain_ckpt/deepseek-ai/DeepSeek-OCR-2/image.png'
@@ -26,8 +20,7 @@ def chat_demo1():
     tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
 
     res = process_vision_info(tokenizer, prompt=prompt, image_file=image_file, base_size = 1024, image_size = 768, 
-                              crop_mode=True, crop_thread=crop_thread, dynamic_preprocess_max_num=dynamic_preprocess_max_num,
-                              add_image_token_id=add_image_token_id)
+                              crop_mode=True, add_image_token_id=add_image_token_id)
 
     model = build_transformer_model(config_path=model_dir, checkpoint_path=model_dir)
     model = model.eval().cuda().to(torch.bfloat16)
